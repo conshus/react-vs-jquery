9eb3b169a3c77132d96d8c460e35014334454f9b
Dwane <Dwane@hiphopandcode.com>
2017-04-24 17:38:40 -0400

    assignment complete

--- src/App.js
+++ src/App.js
@@ -3,16 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: "",
+      photoAdded: false
+    };
+  }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({ text: event.target.value});
+  }
+
+  togglePhoto(event) {
+    this.setState({ photoAdded: !this.state.photoAdded})
+  }
+
+  remainingCharacters(){
+    if (this.state.photoAdded) {
+      return 117 - this.state.text.length;
+    } else {
+      return 140 - this.state.text.length;
+    }
+  }
+
+  overflowAlert(){
+    if (this.remainingCharacters() < 0) {
+      var beforeOverflowText;
+      var overflowText;
+      if (this.state.photoAdded) {
+        beforeOverflowText = this.state.text.substring(107, 117);
+        overflowText = this.state.text.substring(117);
+      } else {
+        beforeOverflowText = this.state.text.substring(130, 140);
+        overflowText = this.state.text.substring(140);
+      }
+      return (
+        <div className="alert alert-warning">
+          <strong>Oops! Too Long: </strong>
+          ...{ beforeOverflowText }
+          <strong className="bg-danger">{ overflowText }</strong>
+        </div>
+      );
+    } else {
+      return "";
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="well clearfix">
+          { this.overflowAlert() }
+          <textarea className="form-control" onChange={this.handleChange.bind(this)}></textarea>
+          <br/>
+          <span className="pull-left">{ this.remainingCharacters() }</span>
+          <button className="btn btn-primary pull-right" disabled={this.remainingCharacters() === 140}>Tweet</button>
+          <button className="btn btn-default pull-right" onClick={this.togglePhoto.bind(this)}>
+            {this.state.photoAdded ? "✓ Photo Added" : "Add Photo"}
+          </button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
